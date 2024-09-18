a5ebc76ab043111670ce3d847fcc034925bb9f69
ioxu <ben@ioxu.com>
2024-09-18 02:58:59 +0100

    log_ui annotations

--- lib/log_ui.lua
+++ lib/log_ui.lua
@@ -1,12 +1,16 @@
 local font_mono_small = love.graphics.newFont( "resources/fonts/SourceCodePro-Regular.ttf", 10 )
 
+--- Logging object to display lines of a scrolling log on-screen.
 --- @class Log_UI
---- @field name string
+--- @field name string a simple identifier for the log
 --- @field log_text love.Text the drawable text love object
---- @field private _line_h number precomputed height of the log font
---- @field private _nlines number a running total of log lines
+--- @field protected _line_h number precomputed height of the log font
+--- @field protected _nlines number a running total of log lines
 local Log_UI = {}
 
+--- Logging function to draw a scrolling log onscreen using LOVE2d graphics.Text.
+--- 
+--- Instanciate with :new() and draw in love.draw with :draw().
 --- @param name string the log's label
 --- @return Log_UI log The log.
 function Log_UI:new(name)
@@ -24,7 +28,8 @@ function Log_UI:n_lines()
     return self._nlines
 end
 
-
+--- add a new line to the log
+--- @param text string string to add to the log, newlines not added automatically
 function Log_UI:log( text )
     self.log_text:add( string.format("%05s", self._nlines) .. " : " .. text, 0, self._line_h * self._nlines )
     self._nlines = self._nlines + 1
